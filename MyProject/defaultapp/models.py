17a7be12bbcd69dff2ab108b289450da9cd5a804
Darshan-Kania <darshankania.ddu0109@gmail.com>
2025-01-20 13:43:08 +0530

    Changed Some Part

--- MyProject/defaultapp/models.py
+++ MyProject/defaultapp/models.py
@@ -2,12 +2,3 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
-
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
